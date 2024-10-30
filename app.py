9b8d738139235a429d2a796aab90e8aaaa6e9df5
DANNY282005 <danielsolomon282005@gmail.com>
2024-10-30 21:18:38 +0530

    Change

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app.secret_key = 'temporary_key'
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='clonedb.cxuy6moa43xw.us-east-1.rds.amazonaws.com',
+        host='clonedb.cqczjv2nkb5f.us-east-1.rds.amazonaws.com',
         user='admin',
-        password='daniel',
+        password='daniel282005@gmail.com',
         database='clone_db'
     )
 
@@ -55,8 +55,8 @@ def login():
 @app.route('/dashboard')
 def dashboard():
     course_urls = [
-        'https://clonebucker1.s3.us-east-1.amazonaws.com/python_code.pdf',
-        'https://clonebbucket.s3.amazonaws.com/PYTHON%2BPhttps://clonebucker1.s3.us-east-1.amazonaws.com/PYTHON%2BPROGRAMMING%2BNOTES.pdf'
+        'https://clonbucket1.s3.us-east-1.amazonaws.com/python_code.pdf',
+        'https://clonbucket1.s3.us-east-1.amazonaws.com/PYTHON%2BPROGRAMMING%2BNOTES.pdf'
     ]
     
     return render_template('dashboard.html', course_urls=course_urls)
